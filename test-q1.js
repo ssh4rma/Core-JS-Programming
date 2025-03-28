12e770393ea37b8406daa4e5eaeb9f928908678a
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-03-28 18:48:03 +0530

    Shubham Sharma 28 March'25 tasks

--- test-q1.js
+++ test-q1.js
@@ -1,25 +1,34 @@
-let str = "baaabbcddffg";
+let str = "baaabbccdffg";
 let N = str.length;
 
-let res = '';
+let res = [];
 
-let vis = new Array(255);
-vis.fill(true);
+let vis = new Array(255); 
 
-for (let i = 0; i < N; ++i) {
+for(let i = 0; i < vis.length; ++i) {
+  vis[i] = true;
+}
+
+for(let i = 0; i < N; ++i) {  
   let c = str[i];
   let freq = 1;
 
-  if (vis[str.charCodeAt(i)]) {
-    for (let j = 0; j < N; ++j) {
-      if (j === i) continue;
-      else if (str[j] === str[i]) freq += 1;
-    }
+  // console.log(vis[str.charCodeAt(i)]);
+  
+  if(vis[str.charCodeAt(i)] == true) {
     vis[str.charCodeAt(i)] = false;
-  }
 
-  if (freq === 1) break;
-  else res += (c + freq);
+    for(let j = 0; j < N; ++j) {
+      if(i === j) continue;
+      if(str[i] === str[j]) freq += 1;
+    }
+  } else continue;
+
+  if(freq == 1) {
+    break;
+  } else {
+    res += (c + freq);
+  }
 }
 
 console.log(res);
