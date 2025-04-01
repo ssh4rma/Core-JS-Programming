ef640ae1a03b92f971e768b35194496c30dec2cf
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 14:58:00 +0530

    Shubham Sharma - getter and setters

--- ES5/ad-hoc-methods.js
+++ ES5/ad-hoc-methods.js
@@ -1,7 +1,9 @@
 //Date.now() : time elapsed since midnight of 1 Jan 1970 in milliseconds
 console.log(Date.now()); 
 
-//toISOString():
+// ISO-8601 standard: YYYY-MM-DDTHH:mm:ss.sssZ:
+// Z means UTC(Zulu) which is +0:00
 
+//toISOString():
 var d = new Date();
 console.log(d.toISOString());
--- ES5/objects-properties.js
+++ ES5/objects-properties.js
@@ -7,3 +7,19 @@ console.log(obj);
 
 var obj = {name:"John", age:30, city:"New York"};
 console.log(JSON.stringify(obj));
+
+//getter and setters
+
+var person = {
+  firstName : "Shubham", 
+  lastName : "Sharma", 
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }, 
+  set changeName(fName) {
+    this.firstName = fName;
+  }
+}
+console.log(person.fullName);
+person.changeName = "aniket";
+console.log(person.fullName);
