c601250fb7137c4e4a937382a86a6ec2f2ce9f79
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 12:55:03 +0530

    Shubham Sharma - trim() string method

--- ES5/string-properties.js
+++ ES5/string-properties.js
@@ -8,3 +8,10 @@ world"
 
 "hello" +
 "world"
+
+//ES5 allows reserved words as property names in object.
+
+//trim(): methods removes whitespaces from both sides of the string
+
+var str = "       Hello World!        ";
+console.log(str.trim());
