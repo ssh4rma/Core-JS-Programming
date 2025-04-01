9ff3f3f542aaa59de26330300a9244467465eecd
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 17:20:57 +0530

    Shubham Sharma - startsWith()

--- ES6/strings.js
+++ ES6/strings.js
@@ -2,3 +2,7 @@
 
 let str = "Hello world, welcome to the universe.";
 console.log(str.includes("world"));
+
+//startsWith(): returns boolean value if the string starts with the specified value.
+
+console.log(str.startsWith("Hello"));
