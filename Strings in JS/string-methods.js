114b0fb5c940bda275ab2e3ee7139bbc0033cc40
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 09:56:42 +0530

    Shubham Sharma - at() string method

--- Strings in JS/string-methods.js
+++ Strings in JS/string-methods.js
@@ -6,3 +6,8 @@ console.log(str.charAt(6));
 //charCodeAt() - returns the code of the char at particular index
 
 console.log(str.charCodeAt(6));
+
+//at() - returns character at the specified index
+//the differnce between at() and charAt() is that at() allows negative index
+
+console.log(str.at(7));
