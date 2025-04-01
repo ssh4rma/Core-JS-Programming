6557fb2ee9d8e1ec61cf4c38ea4fbf52c5eeb101
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 10:33:45 +0530

    Shubham Sharma - search() string method

--- Strings in JS/string-search-method.js
+++ Strings in JS/string-search-method.js
@@ -6,3 +6,10 @@ console.log(text.indexOf("locate"));
 //lastIndexOf() : returns the last index of the occurance of the string in the string and -1 otherwise. 
 
 console.log(text.lastIndexOf("locate"));
+
+
+//both these methods accepts the second arguments of the starting index from where the searching will begin.
+
+//search(): takes the argument as string and return the index of the first occurance of it in the string. It is different from the indexOf() method because it doesn't take starting index of searching.
+
+console.log(search("where"));
