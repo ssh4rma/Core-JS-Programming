1d02974c6641073bd0f6d632c62e9a3333ffb1ce
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 10:27:13 +0530

    Shubham Sharma - indexOf() string search method

---
+++ Strings in JS/string-search-method.js
@@ -0,0 +1,4 @@
+//indexOf() : returns the index of first occurance of the string in the string or returns -1 otherwise. 
+
+let text = "Please locate where 'locate' occurs!";
+console.log(text.indexOf("locate"));
