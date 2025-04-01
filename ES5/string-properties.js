7f6df26a86fc856a3e373213f2678867834a23a4
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 14:33:54 +0530

    Shubham Sharma - some() array method

--- ES5/string-properties.js
+++ ES5/string-properties.js
@@ -62,3 +62,12 @@ var eArr = [45, 4, 9, 16, 25];
 console.log(eArr.every((ele) => {
   return ele > 1;
 }));
+
+// some():This tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.
+//args are same as every() 
+
+var sArr = [45, 4, 9, 16, 25];
+console.log(sArr.some((ele) => {
+  return ele > 18;
+}))
+
