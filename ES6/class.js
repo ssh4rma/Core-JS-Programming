53d6c248a7d4136032004d27d3c2216450935961
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 16:28:46 +0530

    Shubham Sharma - class in JS

---
+++ ES6/class.js
@@ -0,0 +1,15 @@
+class car {
+  constructor(name, brand, isElectric) {
+    this.name = name;
+    this.brand = brand;
+    this.isElectric = isElectric;
+  }
+}
+
+let c1 = new car;
+c1.name = "FS50";
+c1.brand = "FORD";
+c1.isElectric = true;
+
+console.log(c1);
+
