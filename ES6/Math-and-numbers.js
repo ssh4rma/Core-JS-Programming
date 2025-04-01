2494024277e5d23d2f3f4c3fe988175c3b6b6e7e
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 17:51:29 +0530

    Shubham Sharma - Math.sign()

--- ES6/Math-and-numbers.js
+++ ES6/Math-and-numbers.js
@@ -1,3 +1,9 @@
 //Math.trunc(): returns the interger part of the argument.
 console.log(Math.trunc(5.22));
 console.log(Math.trunc(4.2));
+
+//Math.sign(): return the sign of the number means < 0 = -1 , = 0 then 0, and 1 if positive
+
+console.log(Math.sign(-51));
+console.log(Math.sign(0));
+console.log(Math.sign(4));
