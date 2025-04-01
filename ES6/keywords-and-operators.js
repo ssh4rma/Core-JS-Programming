d2a9428c767cf8e6124cc7f311c9a902bb1c1750
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-01 15:40:19 +0530

    Shubham Sharma - const keyword

--- ES6/keywords-and-operators.js
+++ ES6/keywords-and-operators.js
@@ -4,3 +4,11 @@ let x = 10;
   let y = 5;
 }
 console.log(x);
+
+//const keyword: constant declares the constant value, means it points to the memory location of the variable, that's the reason it can't be changed., it's also block scoped.
+
+var z = 10; 
+{
+  const z = 10;
+}
+console.log(z);
