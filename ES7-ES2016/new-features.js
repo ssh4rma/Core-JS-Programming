8020453f657e9a1991729ad4b55279d252b77f8f
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 10:35:22 +0530

    Shubham Sharma - Array.includes() method

--- ES7-ES2016/new-features.js
+++ ES7-ES2016/new-features.js
@@ -2,3 +2,7 @@
 console.log(2 ** 4);
 let x = 4;
 console.log(x **= 2);//exponent assignment operator
+
+//Array.includes(): chech whether the element in the arguments exists in the array or not. - returns boolean value.
+let arr = ["mango", "apple", "orange"];
+console.log(arr.includes("mango"));
