671978d07056b6da4302305a109299f38af7e904
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 10:07:54 +0530

    Shubham Sharma - Math.log10() method

--- ES6/math-methods.js
+++ ES6/math-methods.js
@@ -5,3 +5,7 @@ console.log(Math.cbrt(64));
 //Math.log2(): returns the base 2 log of x
 console.log(Math.log2(2));
 console.log(Math.log2(4));
+
+//Math.log10(): returns the base 10 log of x
+console.log(Math.log10(100));
+console.log(Math.log10(10));
