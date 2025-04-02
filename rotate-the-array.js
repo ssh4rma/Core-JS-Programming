49e39267121ed293346d76ce5bf7c48a97d0b5eb
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 12:46:16 +0530

    Shubham Sharma - rotate the array by k places

---
+++ rotate-the-array.js
@@ -0,0 +1,16 @@
+let arr = [1, 2, 3, 4, 5];
+let k = 1;
+
+k = k % arr.length;
+
+let t = [];
+for(let i = 0; i < k; ++i) {
+  t.push(arr[i]);
+}
+
+arr.splice(0, k);
+
+for(let i = 0; i < t.length; ++i) {
+  arr.push(t[i]); 
+}
+console.log(arr);
