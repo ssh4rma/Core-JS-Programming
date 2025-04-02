01e8d169e5fbd6cd5b95958a9db2bef406644296
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 10:51:42 +0530

    Shubham Sharma - Object.values()

--- ES8-ES2017/objects.js
+++ ES8-ES2017/objects.js
@@ -13,3 +13,6 @@ for(let [key, value] of Object.entries(person)) {
   console.log(key + " is " + value);
 }
 //Helps in iterating over the object.
+
+//Object.values(): It's same as Object.entries() but it returns the 1D array of entries without keys (only values)
+console.log(Object.values(person)); 
