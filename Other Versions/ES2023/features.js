b07be55ec47cc6904c1ed34e9a3cb90e7677d4d7
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 12:15:20 +0530

    Shubham Sharma - with() method

--- Other Versions/ES2023/features.js
+++ Other Versions/ES2023/features.js
@@ -23,3 +23,7 @@ console.log(temp.toSorted());
 //toSpliced(): It's operation is same as Array.splice() method, but it also returns a new array.
 
 console.log(temp.toSpliced(0, 4));
+
+//with() : This method updates the array element without altering the original array. (updates the original array with the value at the specified index and return a new array.)
+
+console.log(temp.with(2, 1000));
