1f59e1c2eb7d02b046f79f741dc32246014dadf4
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 12:01:54 +0530

    Shubham Sharma - lastIndexOf() method

--- Other Versions/ES2023/features.js
+++ Other Versions/ES2023/features.js
@@ -4,3 +4,9 @@ const temp = [27, 28, 30, 40, 42, 35, 30];
 console.log(temp.findLast((x) => {
   return x >= 30;
 }));
+
+//findLastIndex() returns the index of the last element which satisfies the condition.
+
+console.log(temp.lastIndexOf((x) => {
+  return x >= 30;
+}));
