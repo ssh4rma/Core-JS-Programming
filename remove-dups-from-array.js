5b3fdcf3e9ca40c582b4142ed83ae4d2269824f4
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 12:40:50 +0530

    Shubham Sharma - Remove duplicates from array without using extra space

--- remove-dups-from-array.js
+++ remove-dups-from-array.js
@@ -14,12 +14,17 @@ let arr = [1, 2, 2, 3, 4, 4, 5];
 
 // console.log(res);
 
-let i = 1;
-while(i < arr.length) {
-  let j = i;
-  while(j < arr.length && arr[j] == arr[i]) j++;
-  [arr[i], arr[j]] = [arr[j], arr[i]]; 
-  i = j;
-}
 
+let k = 1;
+
+for(let i = 1; i < arr.length; ++i) {
+  let j = i;
+  while(j < arr.length && arr[i] === arr[j]) j++; 
+
+  if(j > i) {
+    arr[k++] = arr[i];  
+    i = j - 1;  
+  }
+}
+arr.splice(k);
 console.log(arr);
