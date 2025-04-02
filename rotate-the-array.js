5d308c134d04bcbe6054e965ff6a9a2eb4d3382c
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-02 12:49:44 +0530

    Shubham Sharma - Optimized rotate the array by k places

--- rotate-the-array.js
+++ rotate-the-array.js
@@ -3,14 +3,21 @@ let k = 1;
 
 k = k % arr.length;
 
-let t = [];
+// let t = [];
+// for(let i = 0; i < k; ++i) {
+//   t.push(arr[i]);
+// }
+
+// arr.splice(0, k);
+
+// for(let i = 0; i < t.length; ++i) {
+//   arr.push(t[i]); 
+// }
+// console.log(arr);
+
+//optimization
 for(let i = 0; i < k; ++i) {
-  t.push(arr[i]);
-}
-
-arr.splice(0, k);
-
-for(let i = 0; i < t.length; ++i) {
-  arr.push(t[i]); 
+  arr.push(arr[i]);
+  arr.shift();
 }
 console.log(arr);
