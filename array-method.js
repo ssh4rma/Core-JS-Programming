04f045c4cd22db1d4ac0f387dc7671e51e6ed21a
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-03 17:44:07 +0530

    splice() array method

--- array-method.js
+++ array-method.js
@@ -5,3 +5,10 @@ console.log(arr1.concat(arr2));
 
 //copyWithin() : args (targetIndex, indexOfDesiredValue)
 console.log(arr1.copyWithin(2, 4));
+
+
+//splice()
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.splice(0, 2, "Mango", "Apple")); 
+console.log(fruits);
