eaba7a4b542bd1441633c54205fc9218e8660608
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-04 11:41:47 +0530

    Symbols implementation as keys of the object

--- practice.js
+++ practice.js
@@ -17,3 +17,26 @@ const sym2 = Symbol(4);
 
 console.log(sym2.description);
 console.log(sym1 == sym2);// false
+
+//symbols implementation in object
+
+
+const sm1 = Symbol("name");
+const sm2 = Symbol("age");
+const sm3 = Symbol("isActive");
+
+const person = {
+  [sm1]: "Alice",          
+  [sm2]: 25,              
+  [sm3]: true,            
+  city: "New York"    
+};
+
+console.log(person[sm1]);  
+console.log(person[sm2]);  
+console.log(person[sm3]);  
+console.log(person.city);   
+
+console.log(Object.keys(person)); //only string keys show up
+
+console.log(Object.getOwnPropertySymbols(person));
