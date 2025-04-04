45f91c49a7b8cfd0f6786df5df1c5b09746a5e50
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-04 14:24:57 +0530

    entries() array method

--- array.js
+++ array.js
@@ -6,7 +6,15 @@ var carr3 = [5,1,56,16,11];
 
 console.log(carr1.concat(carr2.concat(carr3)));
 
-//copyWithin(): 
+//copyWithin(): override the values of the array at the particular index with the target index vlaue.
 
 var copyArr = [1,2,5,5,5,1];
 console.log(copyArr.copyWithin(2, 0));
+
+//entries(): returns the object of array iterator
+
+var enArr = [1,2,4,5,5,11,5,1];
+var ob = enArr.entries();
+for(let i of ob) {
+  console.log(i);
+}
