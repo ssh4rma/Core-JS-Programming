ae0d67a502f7accaaf8eaf126c73592ad5ff3f52
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-04 14:22:14 +0530

    copyWithin() array method

--- array.js
+++ array.js
@@ -1,7 +1,12 @@
 //concat(): this array method is used to join 2 or more strings
-
+//and it returns a new array.
 var carr1 = [1,3,1,51];
 var carr2 = [2,3,5,1,5];
 var carr3 = [5,1,56,16,11];
 
 console.log(carr1.concat(carr2.concat(carr3)));
+
+//copyWithin(): 
+
+var copyArr = [1,2,5,5,5,1];
+console.log(copyArr.copyWithin(2, 0));
