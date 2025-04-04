63dce0d82d09c3cc104d52665bed77197aa2239e
Shubham Sharma <shubham.sharma4390@gmail.com>
2025-04-04 14:11:37 +0530

    concat() array method

---
+++ array.js
@@ -0,0 +1,7 @@
+//concat(): this array method is used to join 2 or more strings
+
+var carr1 = [1,3,1,51];
+var carr2 = [2,3,5,1,5];
+var carr3 = [5,1,56,16,11];
+
+console.log(carr1.concat(carr2.concat(carr3)));
